74a5952737a28d8537b46338082cb2bff64db339
LannonTheCannon <49844413+LannonTheCannon@users.noreply.github.com>
2024-09-23 19:10:32 -0700

    update

--- change_bg.py
+++ change_bg.py
@@ -1,9 +1,7 @@
-# Changing the background
-
 import streamlit as st
 import base64
 
-def add_bg_from_local(image_file):
+def add_bg_from_local(image_file, scale=0.5):
     with open(image_file, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
     st.markdown(
@@ -11,11 +9,14 @@ def add_bg_from_local(image_file):
     <style>
     .stApp {{
         background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
-        background-size: cover
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
     }}
     </style>
     """,
     unsafe_allow_html=True
     )
 
-add_bg_from_local('./images/boy_and_dog.png')
+add_bg_from_local('./images/blanky.png')
---
+++ color_change.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+# Using markdown with HTML and inline CSS
+st.markdown("<h1 style='text-align: center; color: red;'>This is a red title</h1>", unsafe_allow_html=True)
